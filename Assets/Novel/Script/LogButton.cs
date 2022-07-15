7f0276c5775d8b89bdba672045e146a4b36e6029
EnricoGunawan18 <20J70008eg@edu.tca.ac.jp>
2022-07-15 12:22:11 +0900

    more UI

---
+++ Assets/Novel/Script/LogButton.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LogButton : MonoBehaviour
+{
+    [SerializeField]
+    Button button;
+    [SerializeField]
+    GameObject log;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
