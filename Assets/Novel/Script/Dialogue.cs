ea53ddfa064120acd0388b85482ede9e46a47d19
EnricoGunawan18 <20J70008eg@edu.tca.ac.jp>
2022-09-12 13:22:53 +0900

    dialogue

--- Assets/Novel/Script/Dialogue.cs
+++ Assets/Novel/Script/Dialogue.cs
@@ -2,17 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[System.Serializable]
-public class Dialogue
+public class Dialogue : ScriptableObject
 {
+    public int number;
     public string name;
-    [TextArea (3,10)]
-    public string[] sentences;
-    string ui;
-    string se;
-    string bgm;
-    string item;
-    string animation;
-    string effect;
-    string background;
+    public string sentences;
+    public string ui;
+    public string se;
+    public string bgm;
+    public string item;
+    public string animation;
+    public string effect;
+    public string background;
 }
--- Assets/Novel/Script/ReadText.cs
+++ Assets/Novel/Script/ReadText.cs
@@ -6,41 +6,80 @@ using TMPro;
 
 public class ReadText : MonoBehaviour
 {
-    public TextAsset textAsset;
-
     public TextMeshProUGUI dialogueText;
     public TextMeshProUGUI nameText;
 
     public Button nextDialogueButton;
 
+    private Queue<string> names;
     private Queue<string> sentences;
+
+    public TextAsset textAsset;
+
+    string[] data;
+    string[] row;
+    Dialogue d;
+
+    public List<Dialogue> dialogue;
+
     // Start is called before the first frame update
     void Start()
     {
         sentences = new Queue<string>();
+        names = new Queue<string>();
+        dialogue = new List<Dialogue>();
         nextDialogueButton.onClick.AddListener(DisplayNextSentence);
+
+        GetData();
+        StartDialogue();
     }
 
-    public void StartDialogue(Dialogue dialogue)
+    void GetData()
     {
-        nameText.text = dialogue.name;
-
-        sentences.Clear();
-
-        foreach (string sentence in dialogue.sentences)
+        data = textAsset.text.Split(new char[]{'\n'});
+        for(int i = 1; i < data.Length; i++)
         {
-            sentences.Enqueue(sentence);
+            d = new Dialogue();
+            row = data[i].Split(new char[] { ',' });
+            int.TryParse(row[0], out d.number);
+            d.name = row[1];
+            d.sentences = row[2];
+            d.ui = row[3];
+            d.se = row[4];
+            d.bgm = row[5];
+            d.item = row[6]; 
+            d.animation = row[7];
+            d.effect = row[8];
+            d.background = row[9];
+
+            dialogue.Add(d);
+        }
+    }
+    
+    public void StartDialogue()
+    {
+        sentences.Clear();
+        names.Clear();
+        foreach (Dialogue d in dialogue.ToArray())
+        {
+            names.Enqueue(d.name);
+            sentences.Enqueue(d.sentences);
         }
         DisplayNextSentence();
     }
 
     public void DisplayNextSentence()
     {
+        nameText.text = string.Empty;
+        dialogueText.text = string.Empty;
+        
         if(sentences.Count == 0)
         {
             EndDialogue();
             return;
         }
+        string name = names.Dequeue();
+        nameText.text = name;
         string sentence = sentences.Dequeue();
         dialogueText.text = sentence;
     }
